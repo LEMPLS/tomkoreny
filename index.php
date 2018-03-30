0a8e53c94d7e27a7a9a4cb096de9600822294e98
Martin Kapal <flamecze@gmail.com>
2018-03-30 19:12:58 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="robots" content="none">
+    <meta name="description" content="Tomáš Korený is a racing driver from the Czech Republic.">
+    <title>Tomáš Korený</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:700,400" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="wrapper">
+    <div id="logo-url-wrap">
+        <div id="logo-url">
+            <img id="logo" src="img/tomkoreny-logo.png" alt="Tomáš Korený">
+            <div id="site-url">tomkoreny.com</div>
+        </div>
+    </div>
+    <ul id="social">
+        <li>
+            <a href="https://www.facebook.com/tomkoreny">
+                <img class="icon" src="img/social-facebook.png" alt="Follow Tomáš Korený on Facebook">
+                <br>@tomkoreny
+            </a>
+        </li>
+        <li>
+            <a href="https://twitter.com/tomkoreny">
+                <img class="icon" src="img/social-twitter.png" alt="Follow Tomáš Korený on Twitter">
+                <br>@tomkoreny
+            </a>
+        </li>
+        <li>
+            <a href="https://www.instagram.com/tomkoreny/">
+                <img class="icon" src="img/social-instagram.png" alt="Follow Tomáš Korený on Instagram">
+                <br>tomkoreny
+            </a>
+        </li>
+        <li>
+            <a href="https://www.youtube.com/user/pumaslab/videos">
+                <img class="icon" src="img/social-youtube.png" alt="Follow Tomáš Korený on YouTube">
+                <br>pumaslab
+            </a>
+        </li>
+    </ul>
+    <p id="coming-soon">Coming soon&hellip;</p>
+</div>
+
+<div id="page-footer-wrap">
+    <footer id="page-footer">
+        &copy; <?php echo date('Y'); ?> Tomáš Korený<br>
+        Designed by Martin Kapal
+    </footer>
+</div>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-80715868-1', 'auto');
+    ga('send', 'pageview');
+</script>
+
+</body>
+</html>
