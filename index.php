fdd28a6080644a8ae83c6244fe23b0920d243b5f
Martin Kapal <flamecze@gmail.com>
2018-03-30 19:24:05 +0200

    Add robots.txt

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="robots" content="none">
     <meta name="description" content="Tomáš Korený is a racing driver from the Czech Republic.">
     <title>Tomáš Korený</title>
     <link href="https://fonts.googleapis.com/css?family=Lato:700,400" rel="stylesheet">
